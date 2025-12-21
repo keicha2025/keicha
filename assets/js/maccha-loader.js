b0b2a7d00906b41346d32dad5329fb191cdecb72
keicha2025 <wj209ing@gmail.com>
2025-12-21 23:57:20 +0800

    Update maccha-loader.js

--- assets/js/maccha-loader.js
+++ assets/js/maccha-loader.js
@@ -163,7 +163,7 @@ window.addEventListener('load', () => {
             }
 
             section.innerHTML = `
-                <h2 class="text-3xl font-bold text-center mb-10">${brand.name}</h2>
+                <h2 class="text-2xl font-bold text-center mb-10">${brand.name}</h2>
                 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     ${productCardsHTML}
                 </div>`;
